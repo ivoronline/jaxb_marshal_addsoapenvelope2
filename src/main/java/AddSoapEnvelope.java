1c312c09a5625f2c236e9e1cbd5641f00a323e9e
igoreta <ivoronline@gmail.com>
2021-06-01 16:13:50 +0200

    Commit

--- src/main/java/AddSoapEnvelope.java
+++ src/main/java/AddSoapEnvelope.java
@@ -1,9 +1,14 @@
 import org.w3c.dom.Document;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPMessage;
+import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.ByteArrayOutputStream;
@@ -22,11 +27,13 @@ public class AddSoapEnvelope {
     DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
                            documentBuilderFactory.setNamespaceAware(true); //To avoid "local part cannot be "null""
     Document document    = documentBuilderFactory.newDocumentBuilder().parse("src/main/resources/Person.xml");
-    System.out.println(documentToString(document));  //Display original XML
 
     //ADD SOAP ENVELOPE
     SOAPMessage soapMessage = MessageFactory.newInstance().createMessage();
                 soapMessage.getSOAPBody().addDocument(document);
+
+    //CONVERT SOAP MESSAGE TO DOCUMENT
+    Document documentSOAP = SOAPMessagetoDocument(soapMessage); //Not used. Just to demonstrate.
 
     //CONVERT SOAP MESSAGE TO OUTPUT STREAM
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -37,12 +44,25 @@ public class AddSoapEnvelope {
     System.out.println(output); //Display converted XML
 
     //STORE RESULT INTO FILE
-    File              file = new File("PersonSOAP.xml");
+    File              file = new File("src/main/resources/PersonSOAP.xml");
     FileOutputStream  fos = new FileOutputStream(file);
                       fos.write(outputStream.toByteArray());
                       fos.flush();
 
   }
+
+  //=======================================================================================
+  // SOAP MESSAGE TO DOCUMENT
+  //=======================================================================================
+  public static Document SOAPMessagetoDocument(SOAPMessage soapMsg) throws Exception {
+  Source             src         = soapMsg.getSOAPPart().getContent();
+  TransformerFactory tf          = TransformerFactory.newInstance();
+  Transformer        transformer = tf.newTransformer();
+  DOMResult          result      = new DOMResult();
+                     transformer.transform(src, result);
+  Document           document    = (Document) result.getNode();
+  return document;
+}
 
   //=======================================================================================
   // DOCUMENT TO STRING
